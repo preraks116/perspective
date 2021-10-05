2edc0159ab88ab46d9915438fd0ac48a42a0263b
prerak srivastava <prerak.s@research.iiit.ac.in>
2021-10-05 20:38:37 +1200

    ordering files

--- js/about.js
+++ js/about.js
@@ -1,23 +1,44 @@
 var canvas;
 
-let t = 0;;
-let q = 10;;
-
-function setup() {
-    canvas = createCanvas(windowWidth, windowHeight);
-    canvas.position(0, 0);
-    canvas.style('z-index', '-1');
-    background(0);
+function setup(){
+  canvas = createCanvas(windowWidth,windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index','-1');
 }
 
-function draw() {
-    translate(width / 2, height / 2);
-    for(var a = 0; a < TWO_PI; a += 0.1){
-        var r = 100;
-        var x = 100 * cos(a);
-        var y = 100 * sin(a);
-        stroke(255, 255, 255, map(a, 0, TWO_PI, 0, 255));
-        line(x, y, x, y);
-    }
+// the parameter at which x and y depends is usually taken as either t or symbol of theta
+let t = 0;
+let j = 0;
+function draw(){
+  canvas = createCanvas(windowWidth,windowHeight);
+  background(0);
+  translate(width/2,height/2);
+  // stroke('#fff');
+  // strokeWeight(2.5);
+  //loop for adding 100 lines
+  stroke(12,23,133);
+  for(let i = 0;i<69;i++){
+    line(x1(t+i*2),y1(t+i*2),x2(t+i)+20,y2(t+i*3)+20);
+    strokeWeight(i/15);
+  }
+  t+=0.15;
+}
+// function to change initial x co-ordinate of the line
+function x1(t){
+  return sin(t/10)*145+sin(t/20)*145+sin(t/30)*145;
 }
 
+// function to change initial y co-ordinate of the line
+function y1(t){
+  return cos(t/10)*145+cos(t/20)*145+cos(t/30)*145;
+}
+
+// function to change final x co-ordinate of the line
+function x2(t){
+  return sin(t/15)*145+sin(t/25)*145+sin(t/35)*145;
+}
+
+// function to change final y co-ordinate of the line
+function y2(t){
+  return cos(t/15)*145+cos(t/25)*145+cos(t/35)*145;
+}
