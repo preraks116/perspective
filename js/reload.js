7efe0751af5a344713d3c8a3fb9584840b822250
prerak srivastava <prerak.s@research.iiit.ac.in>
2021-11-14 15:03:06 +0530

    added iframe and reset button

---
+++ js/reload.js
@@ -0,0 +1,5 @@
+var frame = document.getElementById("frame");
+
+document.getElementById("reset-button").addEventListener("click", function() {
+    frame.src = frame.src;
+});
---
+++ js/sorting/bubble.js
@@ -0,0 +1,60 @@
+let values = [];
+let i = 0;
+let j = 0;
+
+// The statements in the setup() function
+// execute once when the program begins
+// The array is filled with random values in setup() function.
+function setup() {
+  createCanvas(720, 400);
+  for(let i = 0;i<width/9;i++){
+    values.push(random(height));
+  }
+}
+
+// The statements in draw() function are executed until the
+// program is stopped. Each statement is executed in
+// sequence and after the last line is read, the first
+// line is executed again.
+function draw() {
+  background(0);
+  stroke(255);
+  bubbleSort();
+  simulateSorting();
+}
+
+// The bubbleSort() function sorts taking 8 elements of the array
+// per frame. The algorithm behind this function is 
+// bubble sort.
+function bubbleSort() {
+  for(let k = 0;k<8;k++){
+    if(i<values.length){
+      let temp = values[j];
+      if(values[j] > values[j+1]){
+        values[j] = values[j+1];
+        values[j+1] = temp;
+      }
+      j++;
+      
+      if(j>=values.length-i-1){
+        j = 0;
+        i++;
+      }
+    }
+    else{
+      noLoop();
+    }
+  }
+}
+
+// The simulateSorting() function helps in animating
+// the whole bubble sort algorithm
+// by drawing the rectangles using values
+// in the array as the length of the rectangle.
+function simulateSorting(){
+  for(let i = 0;i<values.length;i++){
+    fill(12,23,133);
+    stroke(255, 255, 255);
+    rect(i*8 , height, 7, -values[i],0);
+   }
+}
